ff0f079079c239d1fc14cd4e40f2ce595aeda687
Chaitanya Ghone <a0393700@ti.com>
2016-09-05 17:07:32 +0530

    [ubuntu] More packages
    Signed-off-by: Chaitanya Ghone <a0393700@ti.com>

--- packages/ubuntu/scripts/install_packages_misc.sh
+++ packages/ubuntu/scripts/install_packages_misc.sh
@@ -3,4 +3,9 @@
 if [ $1 == "install" ]; then
     sudo -E apt-get install dos2unix
     sudo -E apt-get install git
+    sudo -E apt-get install synaptic
+    sudo -E apt-get install x11vnc
+    sudo -E apt-get install vim vim-gnome vim-addon-manager
+    sudo -E apt-get install exuberant-ctags
+    sudo -E apt-get install cscope-el
 fi
