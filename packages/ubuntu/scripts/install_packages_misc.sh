d8c2fa42c71cc6a0db2d34ba2d8059c020d59e75
Chaitanya Ghone <a0393700@ti.com>
2016-09-07 19:13:41 +0530

    [ubuntu] uncrustify added
    Signed-off-by: Chaitanya Ghone <a0393700@ti.com>

--- packages/ubuntu/scripts/install_packages_misc.sh
+++ packages/ubuntu/scripts/install_packages_misc.sh
@@ -3,9 +3,11 @@
 sudo dpkg --add-architecture i386
 
 if [ $1 == "install" ]; then
-	sudo -E apt-get install -y libc6:i386 libncurses5:i386 libstdc++6:i386
+    sudo -E apt-get install -y libc6:i386 libncurses5:i386 libstdc++6:i386
     sudo -E apt-get install -y libncurses5
     sudo -E apt-get install -y dos2unix
+    sudo -E apt-get install -y tofrodos
+    sudo -E apt-get install -y uncrustify
     sudo -E apt-get install -y git
     sudo -E apt-get install -y synaptic
     sudo -E apt-get install -y x11vnc
