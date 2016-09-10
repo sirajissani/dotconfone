2fc53557cc064aa6e8d8ce4b6518ef7fabf62c0b
Chaitanya Ghone <a0393700@ti.com>
2016-09-10 12:25:35 +0530

    [ubuntu] music, ffmpeg updates
    Signed-off-by: Chaitanya Ghone <a0393700@ti.com>

--- packages/ubuntu/scripts/install_packages_ffmpeg.sh
+++ packages/ubuntu/scripts/install_packages_ffmpeg.sh
@@ -2,4 +2,10 @@
 
 if [ $1 == "install" ]; then
     sudo -E apt-get install -y ffmpeg mplayer mencoder mpv feh
+
+    youtube-dl -U
+    if [ $? -ne 0 ]; then
+    	sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+    	sudo chmod a+rx /usr/local/bin/youtube-dl
+    fi
 fi
--- packages/ubuntu/scripts/install_packages_music.sh
+++ packages/ubuntu/scripts/install_packages_music.sh
@@ -5,4 +5,5 @@ sudo add-apt-repository ppa:anonbeat/guayadeque
 if [ $1 == "install" ]; then
     sudo -E apt-get install -y mpd mpc ncmpcpp mpdscribble
     sudo -E apt-get install -y guayadeque
-fi-y
+    sudo -E apt-get install -y conky-all
+fi
