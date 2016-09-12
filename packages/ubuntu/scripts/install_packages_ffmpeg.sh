2f4746c492a2fab31a0e0eeaec62058659a3424a
Chaitanya Ghone <a0393700@ti.com>
2016-09-12 21:46:27 +0530

    [ubuntu] remove youtube-dl via apt and force via curl
    Signed-off-by: Chaitanya Ghone <a0393700@ti.com>

--- packages/ubuntu/scripts/install_packages_ffmpeg.sh
+++ packages/ubuntu/scripts/install_packages_ffmpeg.sh
@@ -3,6 +3,7 @@
 if [ $1 == "install" ]; then
     sudo -E apt-get install -y ffmpeg mplayer mencoder mpv feh
 
+    sudo apt-get remove -y youtube-dl
     youtube-dl -U
     if [ $? -ne 0 ]; then
     	sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
