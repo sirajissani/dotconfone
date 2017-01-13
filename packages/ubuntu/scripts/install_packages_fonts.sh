5880b5f1671789d157c122bb0232d9bc325e38e7
Chaitanya Ghone <csghone@gmail.com>
2017-01-14 01:43:33 +0530

    MPV FFMPEG repo updates
    Signed-off-by: Chaitanya Ghone <csghone@gmail.com>

---
+++ packages/ubuntu/scripts/install_packages_fonts.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+if [ $1 == "install" ]; then
+    sudo -E apt-get install -y fonts-deva*
+	wget https://github.com/madmalik/mononoki/releases/download/1.2/mononoki.zip
+	sudo mkdir -p /usr/local/share/fonts/mononoki/
+	sudo unzip -o mononoki.zip -d /usr/local/share/fonts/mononoki/
+	fc-cache -fv
+fi
+
