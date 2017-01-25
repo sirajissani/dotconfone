69b3bbc282d9355f03b7f0d9387b8a5a4ecf49e8
Chaitanya Ghone <csghone@gmail.com>
2017-01-25 11:37:10 +0530

    [Ubuntu] Minor cleanup, latest cinnamon
    Signed-off-by: Chaitanya Ghone <csghone@gmail.com>

--- packages/ubuntu/scripts/install_packages_desktop.sh
+++ packages/ubuntu/scripts/install_packages_desktop.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+sudo add-apt-repository -y ppa:embrosyn/cinnamon
 
 if [ $1 == "install" ]; then
-	sudo -E apt-get install -y cinnamon-desktop-environment
-	sudo -E apt-get install -y i3 i3-wm
+    sudo -E apt-get install -y cinnamon-desktop-environment
+    sudo -E apt-get install -y i3 i3-wm
 fi
 
--- packages/ubuntu/scripts/install_packages_fonts.sh
+++ packages/ubuntu/scripts/install_packages_fonts.sh
@@ -3,10 +3,13 @@
 
 if [ $1 == "install" ]; then
     sudo -E apt-get install -y fonts-deva*
-	wget https://github.com/madmalik/mononoki/releases/download/1.2/mononoki.zip
-	sudo mkdir -p /usr/local/share/fonts/mononoki/
-	sudo unzip -o mononoki.zip -d /usr/local/share/fonts/mononoki/
-	fc-cache -fv
-	rm mononoki.zip*
+    ls /usr/local/share/fonts/mononoki/mononoki-Regular.ttf
+    if [ $? -ne 0 ]; then
+        wget https://github.com/madmalik/mononoki/releases/download/1.2/mononoki.zip
+        sudo mkdir -p /usr/local/share/fonts/mononoki/
+        sudo unzip -o mononoki.zip -d /usr/local/share/fonts/mononoki/
+        fc-cache -fv
+        rm mononoki.zip*
+    fi
 fi
 
