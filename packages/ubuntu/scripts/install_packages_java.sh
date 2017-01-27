8a6a835225529f68851a3f488ffc0bfd5b1798e1
Chaitanya Ghone <csghone@gmail.com>
2017-01-28 01:40:49 +0530

    [Ubuntu] Java/Webex
    Signed-off-by: Chaitanya Ghone <csghone@gmail.com>

--- packages/ubuntu/scripts/install_packages_java.sh
+++ packages/ubuntu/scripts/install_packages_java.sh
@@ -2,6 +2,7 @@
 
 if [ $1 == "install" ]; then
     sudo -E apt-get install -y openjdk-8-jdk
-    sudo -E apt-get install -y openjdk-8-jre
+    sudo -E apt-get install -y openjdk-8-jre libxmu6 libpangoxft-1.0-0 libpangox-1.0-0 libxv1
+    sudo -E apt-get install -y openjdk-8-jre:i386 libxmu6:i386 libpangoxft-1.0-0:i386 libpangox-1.0-0:i386 libxv1:i386
     sudo -E apt-get install -y icedtea-8-plugin
 fi
