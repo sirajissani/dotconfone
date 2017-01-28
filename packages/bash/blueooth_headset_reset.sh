0ad4c051e8daadf5ad8bb081126037770b286eca
Chaitanya Ghone <csghone@gmail.com>
2017-01-28 10:18:22 +0530

    [Ubuntu] Bluetooth update
    Signed-off-by: Chaitanya Ghone <csghone@gmail.com>

--- packages/bash/blueooth_headset_reset.sh
+++ packages/bash/blueooth_headset_reset.sh
@@ -5,11 +5,12 @@ pulseaudio -k
 sleep 1
 
 #Connect/Disconnect Bluetooth Device
-bluetooth_id=E9:08:EF:D0:5F:1A
-echo $bluetooth_id
-echo disconnect $bluetooth_id | bluetoothctl
+bluetooth_id=`echo devices|bluetoothctl 2>/dev/null| grep ^Device |gawk -F" " '{print $2}'`
+#This should be something like E9:08:F0:C0:5E:2A
+echo Bluetooth Device ID: $bluetooth_id \(This should be something like E9:08:F0:C0:5E:2A\)
+echo disconnect $bluetooth_id | bluetoothctl 2> /dev/null > /dev/null
 sleep 5
-echo connect $bluetooth_id | bluetoothctl
+echo connect $bluetooth_id | bluetoothctl 2> /dev/null > /dev/null
 sleep 10
 
 #Set A2DP_SINK mode
@@ -22,3 +23,6 @@ pacmd set-card-profile $index a2dp_sink
 #Set default SINK to bluez
 sink_name=`pacmd list-cards |grep bluez -B1 |grep sinks -A1 |grep -v "sinks:" |gawk -F"/" '{ print $1 }'`
 pacmd set-default-sink $sink_name
+
+echo ""
+echo "____If this does not work, reboot your headset and run this script again____"
