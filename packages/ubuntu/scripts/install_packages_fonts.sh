755b9788608a98f848b1aaac67e150c721a5c36e
Chaitanya Ghone <csghone@gmail.com>
2017-02-08 09:28:25 +0530

    [Fonts] Add monaco, make script generic
    Signed-off-by: Chaitanya Ghone <csghone@gmail.com>

--- packages/ubuntu/scripts/install_packages_fonts.sh
+++ packages/ubuntu/scripts/install_packages_fonts.sh
@@ -1,15 +1,49 @@
 #!/bin/bash
 
+function install_font()
+{
+    #$1 => Folder Name in /usr/local/share/fonts
+    #$2 => Link to download
+
+    font_dir=/usr/local/share/fonts/$1
+
+    ls $font_dir
+    if [ $? -ne 0 ]; then
+        #Make directory
+        sudo mkdir -p $font_dir
+
+        #Download file
+        wget $2 -O temp
+
+        #Check if it is zip
+        file temp |grep Zip
+        if [ $? -eq 0 ]; then
+            sudo unzip -o temp -d $font_dir
+            rm -f temp
+        fi
+
+        #Check if it is TTF
+        file temp |grep TrueType
+        if [ $? -eq 0 ]; then
+            sudo cp temp $font_dir/$1.ttf
+            rm -f temp
+        fi
+
+        ls temp
+        if [ $? -eq 0 ]; then
+             echo "Unknown file, removing it"
+             rm -f temp
+        fi
+
+    fi
+}
 
 if [ $1 == "install" ]; then
     sudo -E apt-get install -y fonts-deva*
-    ls /usr/local/share/fonts/mononoki/mononoki-Regular.ttf
-    if [ $? -ne 0 ]; then
-        wget https://github.com/madmalik/mononoki/releases/download/1.2/mononoki.zip
-        sudo mkdir -p /usr/local/share/fonts/mononoki/
-        sudo unzip -o mononoki.zip -d /usr/local/share/fonts/mononoki/
-        fc-cache -fv
-        rm mononoki.zip*
-    fi
+
+    install_font mononoki "https://github.com/madmalik/mononoki/releases/download/1.2/mononoki.zip"
+    install_font Monaco "https://raw.githubusercontent.com/todylu/monaco.ttf/master/monaco.ttf"
+
+    fc-cache -fv
 fi
 
