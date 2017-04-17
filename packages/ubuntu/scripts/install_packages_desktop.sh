ac1ddbb4c6fb55da63893e242677c7dc526274fe
Siraj Issani <siraj.issani@gmail.com>
2017-04-17 14:26:31 +0530

    feat(i3wm): update repo for latest-stable i3

--- packages/ubuntu/scripts/install_packages_desktop.sh
+++ packages/ubuntu/scripts/install_packages_desktop.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 if [ $1 == "install" ]; then
+    # On Ubuntu systems >= 16.04
+    /usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2017.01.02_all.deb keyring.deb SHA256:4c3c6685b1181d83efe3a479c5ae38a2a44e23add55e16a328b8c8560bf05e5f
+    sudo -E apt install ./keyring.deb
+    sudo -E echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" >> /etc/apt/sources.list.d/sur5r-i3.list
+    sudo -E apt update
     sudo -E apt-get install -y i3 i3-wm
 fi
 
