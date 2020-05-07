7e59a80fc1255341704ce08759655122de969da8
Siraj Issani <siraj.issani@siemens.com>
2020-05-07 17:02:53 +0530

    Remove Gnome stuff from init, fix Feh background

--- packages/i3/displays.sh
+++ packages/i3/displays.sh
@@ -4,5 +4,5 @@ intern=LVDS-0
 extern=DP-3
 
 if xrandr | grep "$extern connected"; then
-    xrandr --output $extern --primary --dpi 150 --auto --output $intern --off
+    xrandr --output $extern --primary --dpi 144 --auto --output $intern --off
 fi
